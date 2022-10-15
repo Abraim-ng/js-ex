339db4f63e13ab942d6b83b82d0916f55225a52e
Abraim-ng <abraimmostapha@yahoo.com>
2022-10-15 08:38:29 +0100

    fc

---
+++ script.js
@@ -0,0 +1,15 @@
+let person = {
+    FirstName: "Mostapha",
+    LastName: "Abraim",
+    Age: "22",
+    Adresse: "Morocco, Agadir",
+}
+
+let Car = {
+    CarName : "Seat",
+    Color : "Gray",
+    Model : "Seat-fr",
+    Year : "2022",
+}
+
+alert(Car.CarName)
